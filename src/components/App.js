00e83fdd1c0897a96f76a984dbdcb0855a4f3cfd
Satyam <nikamsatyam187@gmail.com>
2025-12-09 11:34:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,49 +2,33 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import '../styles/App.css';
 
-// Home Component
-const Home = () => {
-  return (
-    <div>
-      <h1>Welcome to my website!</h1>
-    </div>
-  );
-};
-
-// About Component
-const About = () => {
-  return (
-    <div>
-      <h1>This is a sample React Router program.</h1>
-    </div>
-  );
-};
-
-// Navigation Component
-const Navigation = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-};
-
-// Main App Component
 const App = () => {
   return (
     <Router>
-      <div className="App">
-        <Navigation />
+      <div className="app">
+        <nav className="nav">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/" element={
+            <div className="page">
+              <h1>Welcome to my website!</h1>
+            </div>
+          } />
+          
+          <Route path="/about" element={
+            <div className="page">
+              <h1>This is a sample React Router program.</h1>
+            </div>
+          } />
         </Routes>
       </div>
     </Router>
