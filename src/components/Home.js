bc505a8edf63b54b756e4b077302ef69ff99d9fc
Satyam <nikamsatyam187@gmail.com>
2025-12-09 11:37:28 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,12 @@
 
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="page home-page">
+      <h1>Welcome to my website!</h1>
+    </div>
+  );
+};
+
+export default Home;
